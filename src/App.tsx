b8fa2622decf4d26d71a31615ec8f5558800030c
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-09-28 10:44:10 +0300

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Providers from "providers/Providers";
+
+import "styles/base.css";
+
+function App() {
+  return <Providers>App</Providers>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        svgo: false,
+        ref: true,
+        titleProp: false,
+      },
+      include: "**/*.svg",
+    }),
+  ],
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "./src/assets"),
+      components: path.resolve(__dirname, "./src/components"),
+      contexts: path.resolve(__dirname, "./src/contexts"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      i18n: path.resolve(__dirname, "./src/i18n"),
+      mocks: path.resolve(__dirname, "./src/mocks"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      providers: path.resolve(__dirname, "./src/providers"),
+      store: path.resolve(__dirname, "./src/store"),
+      styles: path.resolve(__dirname, "./src/styles"),
+      types: path.resolve(__dirname, "./src/types"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
+});
